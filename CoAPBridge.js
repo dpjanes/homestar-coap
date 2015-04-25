878e9cdc408a9961a0825a56476fadabdf295fdc
David Janes <davidjanes@davidjanes.com>
2015-04-25 10:28:43 -0400

    subclass from iotdb.Bridge

--- CoAPBridge.js
+++ CoAPBridge.js
@@ -63,6 +63,12 @@ var CoAPBridge = function (initd, native) {
     }
 };
 
+CoAPBridge.prototype = new iotdb.Bridge();
+
+CoAPBridge.prototype.name = function () {
+    return "CoAPBridge";
+};
+
 /* --- lifecycle --- */
 
 /**

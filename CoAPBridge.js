fd77a4f607c3bdc1c5af29bbaa1c0c2571359454
David Janes <davidjanes@davidjanes.com>
2016-02-06 10:26:51 -0500

    fixed up 'push' validation

--- CoAPBridge.js
+++ CoAPBridge.js
@@ -154,7 +154,7 @@ CoAPBridge.prototype.push = function (pushd, done) {
         return;
     }
 
-    self._validate_push(pushd);
+    self._validate_push(pushd, done);
 
     logger.info({
         method: "push",
